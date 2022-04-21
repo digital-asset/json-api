36328e5b6d681a601dfc0cf3f53bcd80b2a95f31
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2022-04-21 21:20:52 +0200

    do not fix Bazel cache
    Cache maintenance can be done from the Bazel repo if need be. Hopefully
    we won't be using Bazel long enough for the cache corruption bug to
    affect us here, especially since it seems to mostly appear on Windows.
    
    CHANGELOG_BEGIN
    CHANGELOG_END

--- ci/cron/src/Main.hs
+++ ci/cron/src/Main.hs
@@ -3,7 +3,6 @@
 
 module Main (main) where
 
-import qualified BazelCache
 import qualified CheckReleases
 import qualified Docs
 
@@ -15,13 +14,11 @@ data CliArgs = Docs
              | Check { bash_lib :: String,
                        gcp_credentials :: Maybe String,
                        max_releases :: Maybe Int }
-             | BazelCache BazelCache.Opts
 
 parser :: Opt.ParserInfo CliArgs
 parser = info "This program is meant to be run by CI cron. You probably don't have sufficient access rights to run it locally."
               (Opt.hsubparser (Opt.command "docs" docs
-                            <> Opt.command "check" check
-                            <> Opt.command "bazel-cache" bazelCache))
+                            <> Opt.command "check" check))
   where info t p = Opt.info (p Opt.<**> Opt.helper) (Opt.progDesc t)
         docs = info "Build & push latest docs, if needed."
                     (pure Docs)
@@ -37,30 +34,6 @@ parser = info "This program is meant to be run by CI cron. You probably don't ha
                                   Opt.option Opt.auto (Opt.long "max-releases"
                                          <> Opt.metavar "INT"
                                          <> Opt.help "Max number of releases to check.")))
-        bazelCache =
-            info "Bazel cache debugging and fixing." $
-            fmap BazelCache $ BazelCache.Opts
-              <$> fmap (\m -> fromInteger (m * 60)) (Opt.option Opt.auto
-                       (Opt.long "age" <>
-                        Opt.help "Maximum age of entries that will be considered in minutes")
-                    )
-              <*> Opt.optional
-                    (Opt.strOption
-                      (Opt.long "cache-suffix" <>
-                      Opt.help "Cache suffix as set by ci/configure-bazel.sh"))
-              <*> Opt.option Opt.auto
-                    (Opt.long "queue-size" <>
-                     Opt.value 128 <>
-                     Opt.help "Size of the queue used to distribute tasks among workers")
-              <*> Opt.option Opt.auto
-                    (Opt.long "concurrency" <>
-                     Opt.value 32 <>
-                     Opt.help "Number of concurrent workers that validate AC entries")
-              <*> fmap BazelCache.Delete
-                    (Opt.switch
-                       (Opt.long "delete" <>
-                        Opt.help "Whether invalid entries should be deleted or just displayed"))
-
 
 main :: IO ()
 main = do
@@ -73,4 +46,3 @@ main = do
           Docs.docs Docs.damlOnSqlDocOpts
       Check { bash_lib, gcp_credentials, max_releases } ->
           CheckReleases.check_releases gcp_credentials bash_lib max_releases
-      BazelCache opts -> BazelCache.run opts
